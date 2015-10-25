736c7e8a005abd8d1779767643b868dbf57d42ad
GuanJ <guan_jun@live.cn>
2015-10-25 22:40:00 +0800

    lecture2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -26,19 +26,30 @@ class ViewController: UIViewController {
 
     
     @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
+       
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
-        switch operation{
-        case "×": performOperation {$0 * $1}
-        case "+": performOperation {$0 + $1}
-        case "-": performOperation {$1 - $0}
-        case "÷": performOperation {$1 / $0}
-        default: break
+        if let operation = sender.currentTitle {
+            switch operation{
+            case "×": performOperation {$0 * $1}
+            case "+": performOperation {$0 + $1}
+            case "-": performOperation {$1 - $0}
+            case "÷": performOperation {$1 / $0}
+            case "√": performOperation2 {sqrt($0)}
+            default: break
+            }
+        }
+        
+    }
+    
+    func performOperation2(operation: Double ->Double){
+        if operandStack.count >= 1{
+            displayValue = operation(operandStack.removeLast())
+            enter()
         }
     }
-
+    
     func performOperation(operation: (Double,Double) ->Double){
         if operandStack.count >= 2{
             displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
